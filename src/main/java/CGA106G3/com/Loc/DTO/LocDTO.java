96c169901398bb547bb464b8547c947cf0573116
sandra hung <pig801121@gmail.com>
2023-03-28 20:25:16 +0800

    18

---
+++ src/main/java/CGA106G3/com/Loc/DTO/LocDTO.java
@@ -0,0 +1,15 @@
+package CGA106G3.com.Loc.DTO;
+
+import lombok.Data;
+
+@Data
+public class LocDTO {
+
+    private Integer locno;
+
+    private String locname;
+
+    private String locpur;
+
+    private Integer locsta;
+}
---
+++ src/main/java/CGA106G3/com/Loc/Repository/LocRepository.java
@@ -0,0 +1,9 @@
+package CGA106G3.com.Loc.Repository;
+
+import CGA106G3.com.Loc.DTO.LocDTO;
+import CGA106G3.com.Loc.Entity.Loc;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocRepository extends JpaRepository <Loc,Integer> {
+
+}
