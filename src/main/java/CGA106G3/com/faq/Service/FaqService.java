ea481feb25e42c498e943aa642fba666a5593d5a
K <alex918855@gmail.com>
2023-04-12 19:23:43 +0800

    add faq model

--- src/main/java/CGA106G3/com/faq/Service/FaqService.java
+++ src/main/java/CGA106G3/com/faq/Service/FaqService.java
@@ -1,4 +1,141 @@
 package CGA106G3.com.faq.Service;
 
+
+import CGA106G3.com.faq.DTO.FaqDTO;
+import CGA106G3.com.faq.Entity.Faq;
+import CGA106G3.com.faq.repository.FaqRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+
 public class FaqService {
+    private static final String Key = "faq:";
+
+
+    @Autowired
+    private RedisTemplate<String , Faq> redisTemplate;
+
+    @Autowired
+    private FaqRepository faqRepository;
+
+
+//    public void setFaq(String faq, String faqname){
+//        redisTemplate.opsForValue().set(faq,faqname);
+//    }
+//    public String getFaqname(String faq){
+//        return redisTemplate.opsForValue().get(faq);
+//    }
+//
+//    public Faq addFaq(Faq faq){
+//        redisTemplate.opsForValue().set(faq.getFaqname().toString(), String.valueOf(faq));
+//
+//        return faqRepository.save(faq);
+//    }
+
+
+
+
+
+
+
+
+
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public boolean isFaqNameExist(String faqname){
+
+        return faqRepository.findByFaqname(faqname) != null;
+    }
+
+    public Faq addFaq(Faq faq){
+        Faq savedFaq = faqRepository.save(faq);
+        redisTemplate.opsForValue().set(Key + faq.getFaqno(), savedFaq);
+        return savedFaq;
+    }
+
+    public Faq updateFaq(Faq faq){
+        Faq updatedFaq = faqRepository.save(faq);
+        redisTemplate.opsForValue().set(Key + faq.getFaqno(), updatedFaq);
+        return updatedFaq;
+    }
+
+    public Optional<Faq> findFaqById(Integer faqno){
+        String key = Key + faqno;
+        Faq faq = redisTemplate.opsForValue().get(key);
+        if (faq == null){
+            Optional<Faq> optionalFaq = faqRepository.findById(faqno);
+            optionalFaq.ifPresent(f -> redisTemplate.opsForValue().set(key, f));
+            return optionalFaq;
+        }
+        return Optional.of(faq);
+    }
+
+    public Optional<Faq> findByFaqname(String faqname){
+        return faqRepository.findByFaqname(faqname);
+    }
+    public List<FaqDTO> getAllFaq(){
+        String key = Key + "*";
+        Set<String> keys = redisTemplate.keys(key);
+        if (keys == null || keys.isEmpty()){
+            List<Faq> faqs = faqRepository.findAll();
+
+//            faqs.forEach(faq -> redisTemplate.opsForValue().set(Key + faq.getFaqno(), faq));
+            return faqs.stream().map(this::EntityToDTO).collect(Collectors.toList());
+        }
+        return redisTemplate.opsForValue().multiGet(keys).stream().map(this::EntityToDTO).collect(Collectors.toList());
+
+    }
+    private FaqDTO EntityToDTO(Faq faq){
+      FaqDTO faqDTO = modelMapper.map(faq, FaqDTO.class);
+        return faqDTO;
+    }
+
+
+
+
+//    public Faq saveFaq(String faq){
+//        redisTemplate.opsForValue().set(faq.getFaqname().toString(), faq);
+//
+//        return faqRepository.save(faq);
+//    }
+//    public Faq getFaqname(Object faqname){
+//
+//        Faq faq = redisTemplate.opsForValue().get(faqname.toString());
+//
+//        if (faq == null){
+//
+//            Optional<Faq> optionalFaq = faqRepository.findByFaqname(faqname);
+//
+//            if (optionalFaq.isPresent()){
+//                faq = optionalFaq.get();
+//
+//                redisTemplate.opsForValue().set(faq.getFaqname().toString(), faq);
+//            }
+//        }
+//        return faq;
+//    }
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/CGA106G3/com/faq/Service/FaqServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package CGA106G3.com.faq.Service;
-
-import CGA106G3.Core.Service.ServiceCore;
-
-public interface FaqServiceImpl extends ServiceCore {
-
-
-
-}
